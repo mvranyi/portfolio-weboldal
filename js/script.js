e1c0b2a1d560fd223c3c4e102412161c4b66ed92
mvranyi <muranyikende@gmail.com>
2024-10-08 19:07:00 +0200

    kilencedik commit
    véglegesítés

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-// Várjuk meg, hogy a dokumentum teljesen betöltődjön
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Lekérés a randomuser.me API-ról
+    
     fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(data => {
-            // A felhasználói adatokat tartalmazó objektum
+            
             const user = data.results[0];
             
-            // HTML elem, ahová a felhasználó adatai kerülnek
+            
             const userInfoDiv = document.getElementById('user-info');
 
-            // Generáljuk a HTML-t a felhasználó adataival
+            
             userInfoDiv.innerHTML = `
                 <img src="${user.picture.large}" alt="Véletlenszerű felhasználó képe">
                 <p><strong>Név:</strong> ${user.name.first} ${user.name.last}</p>
